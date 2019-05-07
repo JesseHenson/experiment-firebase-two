57b855957d9342d7b6341a03f6258511e1d6ac5b
Jesse Henson <29315680+JesseHenson@users.noreply.github.com>
2019-05-07 07:59:58 -0600

    asf

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -1,10 +1,11 @@
 import app from "firebase/app";
 import "firebase/auth";
+import "firebase/database";
 
 const config = {
   apiKey: "AIzaSyBvseaPORG5iq7fUwwfXM3hc_dUDtbxXTs",
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  databaseURL: "https://experiment-51294.firebaseio.com",
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
@@ -15,6 +16,7 @@ class Firebase {
     app.initializeApp(config);
 
     this.auth = app.auth();
+    this.database = app.database();
   }
 
   // *** Auth API ***
